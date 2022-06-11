dbe9209f8818cea2556ab385c9deb6ab395f2e1b
pseudo-aloha <b07901075@ntu.edu.tw>
2022-06-11 18:45:17 +0800

    data changed value with key

--- data/path_datagen.py
+++ data/path_datagen.py
@@ -1,9 +1,22 @@
 import jsonlines
 import csv
+import json
+
+def get_key(val, my_dict):
+    for key, value in my_dict.items():
+        if value == val:
+            return key
+    return "errrrrr!!!! key not found" 
 
 # phase1
 in_file_names = ["augfp4_in_domain_train_out_goldkeywords.jsonl", "augfp4_in_domain_valid_fromtrain.jsonl", "augfp4_out_of_domain_train_out_goldkeywords.jsonl", "augfp4_out_of_domain_dev_out_goldkeywords.jsonl"]
 out_file_names = ["in_domain/train/phase1_text.csv", "in_domain/dev/phase1_text.csv", "out_of_domain/train/phase1_text.csv", "out_of_domain/dev/phase1_text.csv"]
+
+
+g = open("relation2text.json")
+relation = json.load(g)
+# print(type(relation))
+key_found = False
 
 for i in range(4):
     data = []
@@ -18,7 +31,20 @@ for i in range(4):
         for d in data:
             if idx != 0:
                 inputs = "context : " + d["context"] + " @ path_tailentity : " + d["path_tailentity"]
-                target = d["path"]
+                path = d["path"]
+                new_path = ""
+                for word in path.split():
+                    # print(word)
+                    key_found = False
+                    for val in relation.values():
+                        if word == val:
+                            new_path += get_key(val, relation) + " "
+                            key_found = True
+                    if key_found:
+                        continue
+                    else:
+                        new_path += word + " "
+                target = new_path[:-1]
                 writer.writerow([inputs, target])
             idx += 1
 
@@ -38,7 +64,21 @@ for i in range(4):
         writer.writerow(["inputs", "target"])
         for d in data:
             if idx != 0:
-                inputs = "context : " + d["context"] + " @ path_tailentity : " + d["path_tailentity"] + " @ path : " + d["path"]
+                path = d["path"]
+                new_path = ""
+                for word in path.split():
+                    # print(word)
+                    key_found = False
+                    for val in relation.values():
+                        if word == val:
+                            new_path += get_key(val, relation) + " "
+                            key_found = True
+                            # print("word = ", word, "key = ", get_key(val, relation))
+                    if key_found:
+                        continue
+                    else:
+                        new_path += word + " "
+                inputs = "context : " + d["context"] + " @ path_tailentity : " + d["path_tailentity"] + " @ path : " + new_path[:-1]
                 target = d["response"]
             idx += 1
             writer.writerow([inputs, target])
