dd08c9863700b51fc41840d4e121a8449bb53067
JeterHwang <jeter0309@gmail.com>
2022-06-13 21:50:29 +0800

    add docunent

--- T5/run.sh
+++ T5/run.sh
@@ -4,4 +4,4 @@ python3 -m spacy en_core_web_sm
 chmod 777 download.sh
 ./download.sh
 
-python3 simulator.py --split test --device cuda:0
+python3 simulator.py --disable_output_dialog --num_chats 980 --device cuda:0 --split test
